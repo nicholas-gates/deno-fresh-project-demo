3e03b814af552f6682eeae1c971c2f0c5bac36ec
Nicholas Gates <nickcgates@outlook.com>
2024-04-19 18:12:50 -0400

    Refactor deno.json and crewai.ts files to include OpenAI functionality

--- routes/api/crewai.ts
+++ routes/api/crewai.ts
@@ -1,12 +1,44 @@
 import { Handlers } from "$fresh/server.ts";
 
-import { ChatOpenAI } from "https://esm.sh/@langchain/openai";
+import { ChatOpenAI, OpenAI } from "https://esm.sh/@langchain/openai";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { StringOutputParser } from "@langchain/core/output_parsers";
+import {
+  RunnableConfig,
+  RunnableInterface,
+} from "https://esm.sh/v135/@langchain/core@0.1.57/runnables.js";
+import { ChatPromptValueInterface } from "https://esm.sh/v135/@langchain/core@0.1.57/dist/prompt_values.js";
+import { AIMessageChunk } from "https://esm.sh/v135/@langchain/core@0.1.57/messages.js";
+
+// const getOpenAiModel = (): ChatOpenAI => {
+//   return new ChatOpenAI({
+//     apiKey: Deno.env.get("OPENAI_API_KEY")!,
+//   });
+// }
+
+type EnumModelNames = "OpenAi" | "OpenAiChat";
+
+const getModel = (modelName: EnumModelNames): OpenAI | ChatOpenAI => {
+  switch (modelName) {
+    case "OpenAi":
+      return new OpenAI({
+        apiKey: Deno.env.get("OPENAI_API_KEY")!,
+      });
+    case "OpenAiChat":
+      return new ChatOpenAI({
+        apiKey: Deno.env.get("OPENAI_API_KEY")!,
+      });
+    default:
+      return new ChatOpenAI({
+        apiKey: Deno.env.get("OPENAI_API_KEY")!,
+      });
+  }
+};
 
 export const handler: Handlers = {
   async GET(_req) {
-    const apiresponse = await getJokeResponse();
+    // const apiresponse = await getJokeResponse();
+    const apiresponse = await getInvokeLlmResponse();
 
     return new Response(JSON.stringify(apiresponse), {
       headers: { "Content-Type": "application/json" },
@@ -19,7 +51,8 @@ const getJokeResponse = async () => {
     ["human", "Tell me a short joke about {topic}"],
   ]);
 
-  const model = getOpenAiModel();
+  // const model = getOpenAiModel();
+  const model = getModel("OpenAiChat");
 
   const outputParser = new StringOutputParser();
 
@@ -37,8 +70,18 @@ const getJokeResponse = async () => {
   return apiresponse;
 };
 
-const getOpenAiModel = () => {
-  return new ChatOpenAI({
-    apiKey: Deno.env.get("OPENAI_API_KEY")!,
-  });
-}
+const getInvokeLlmResponse = async () => {
+  const model = getModel("OpenAi");
+
+  const prompt = "What would be a good company name a company that makes colorful socks?"
+
+  //Calls out to the model's (OpenAI's) endpoint passing the prompt. This call returns a string
+  const response = await model.invoke(
+    prompt,
+  );
+
+  return {
+    prompt,
+    response
+  }
+};
